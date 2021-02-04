f377e86f8ae8cc9a67607e3b69f922da89d32b05
“zed” <zed@kolk.me>
2021-02-04 16:06:46 -0600

    finished

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,11 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+    # byebug
+  end
+
+
 end
